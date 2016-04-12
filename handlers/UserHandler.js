a8b7ee06cf4e6b33b90a2059a76bd612bfb26b9b
coolspan <coolspan@sina.cn>
2016-04-12 23:39:17 +0800

    add ssdb test cases.

--- handlers/UserHandler.js
+++ handlers/UserHandler.js
@@ -4,11 +4,29 @@
 
 var userHanlder = {};
 
+
+var SSDB = require('./SSDB');
+var ssdb = SSDB.connect("127.0.0.1", "8888", 30 * 1000,function(){
+    console.error("-----------------------")
+});
 userHanlder.getInfo = function (req, res, next) {
     //console.log(req.params)//URL
     console.log(req.body)//POST
     console.log(req.query)//GET
+
+    ssdb.set('a', new Date(), function () {
+        console.log('set a');
+    });
+
+    ssdb.get('a', function (err, val) {
+        console.log('get a = ' + val);
+        //ssdb.close();
+    });
+
+
     res.json({"name": req.query.name})
     //next()
 }
+
+
 module.exports = userHanlder;
